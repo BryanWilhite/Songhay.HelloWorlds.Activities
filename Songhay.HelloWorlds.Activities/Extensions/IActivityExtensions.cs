f01b4ff3bcf2def8227e23b5bf5650746eb96896
Bryan Wilhite <rasx@songhaysystem.com>
2017-10-22 10:33:40 -0700

    added args extensions

--- Songhay.HelloWorlds.Activities/Extensions/IActivityExtensions.cs
+++ Songhay.HelloWorlds.Activities/Extensions/IActivityExtensions.cs
@@ -26,7 +26,34 @@ namespace Songhay.HelloWorlds.Activities.Extensions
             if (string.IsNullOrEmpty(activityName)) throw new ArgumentNullException("The expected Activity name is not here.");
             if (!activities.Keys.Contains(activityName)) throw new ArgumentNullException($"The expected Activity name, {activityName}, is not here.");
 
-            return activities[activityName].Value;
+            var activity = activities[activityName].Value;
+            if (activity == null) throw new NullReferenceException("The expected Activity is not here.");
+
+            return activity;
+        }
+
+        /// <summary>
+        /// Converts <c>args</c> to the name of the Activity.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static string[] ToActivityArgs(this string[] args)
+        {
+            if (args?.Count() < 2) return args;
+
+            return args.Skip(1).ToArray();
+        }
+
+        /// <summary>
+        /// Converts <c>args</c> to the name of the Activity.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">The expected Activity name is not here.</exception>
+        public static string ToActivityName(this IEnumerable<string> args)
+        {
+            if (args?.Count() < 1) throw new ArgumentException("The expected Activity name is not here.");
+            var activityName = args.ElementAt(0);
+            return activityName;
         }
     }
 }
--- Songhay.HelloWorlds.Shell/Program.cs
+++ Songhay.HelloWorlds.Shell/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Songhay.HelloWorlds.Activities;
-using System;
-using System.Linq;
+using Songhay.HelloWorlds.Activities.Extensions;
 
 namespace Songhay.HelloWorlds.Shell
 {
@@ -8,13 +7,9 @@ namespace Songhay.HelloWorlds.Shell
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1) throw new ArgumentException("The expected Activity name is not here.");
-
-            var activityName = args[0];
-            var activitiesGetter = new ActivitiesGetter();
-            var activity = activitiesGetter.GetActivity(activityName);
-            if (activity == null) throw new NullReferenceException("The expected Activity is not here.");
-            activity.Start(args.Skip(1).ToArray());
+            var activityName = args.ToActivityName();
+            var activity = (new ActivitiesGetter()).GetActivity(activityName);
+            activity.Start(args.ToActivityArgs());
         }
     }
 }
