e0b02d6fecac6e5367bbab78c7cdc7e9d7c7ee70
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-12-03 13:19:40 -0800

    :fire: :tractor: :umbrella: :sparkles: resolves #1

---
+++ Songhay.HelloWorlds.Functions.Tests/DurableFunctionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Songhay.HelloWorlds.Functions.Tests
+{
+    public class DurableFunctionsTests
+    {
+        public DurableFunctionsTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData("/OrchestrationTrigger", "GetHelloWorldOutputActivity Mercury Venus Earth Mars Jupiter")]
+        public async Task OrchestrationTrigger_Test(string path, string args)
+        {
+            // arrange
+            var uri = new Uri($"{LOCAL}{path}", UriKind.Absolute);
+            var message = new HttpRequestMessage(HttpMethod.Post, uri);
+
+            //act
+            this._testOutputHelper.WriteLine($"{nameof(uri)}: {uri.OriginalString}");
+            var jO = JObject.FromObject(new { args });
+            var response = await message.SendBodyAsync(jO.ToString());
+
+            // assert
+            Assert.Equal(HttpStatusCode.Accepted, response.StatusCode);
+        }
+
+        const string LOCAL = "http://localhost:7071/api";
+
+        readonly ITestOutputHelper _testOutputHelper;
+    }
+}
---
+++ Songhay.HelloWorlds.Functions.Tests/StatelessFunctionsTests.cs
@@ -0,0 +1,56 @@
+using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Songhay.HelloWorlds.Functions.Tests
+{
+    public class StatelessFunctionsTests
+    {
+        public StatelessFunctionsTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData("/ActivityTrigger", "GetHelloWorldOutputActivity Jupiter")]
+        public async Task ActivityTrigger_Test(string path, string args)
+        {
+            // arrange
+            var uri = new Uri($"{LOCAL}{path}", UriKind.Absolute);
+            var message = new HttpRequestMessage(HttpMethod.Post, uri);
+
+            //act
+            this._testOutputHelper.WriteLine($"{nameof(uri)}: {uri.OriginalString}");
+            var jO = JObject.FromObject(new {args});
+            var response = await message.SendBodyAsync(jO.ToString());
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/HttpTrigger?name=Bill")]
+        public async Task HttpTrigger_Test(string path)
+        {
+            // arrange
+            var uri = new Uri($"{LOCAL}{path}", UriKind.Absolute);
+            var message = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            //act
+            this._testOutputHelper.WriteLine($"{nameof(uri)}: {uri.OriginalString}");
+            var response = await message.SendAsync();
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        const string LOCAL = "http://localhost:7071/api";
+
+        readonly ITestOutputHelper _testOutputHelper;
+    }
+}
