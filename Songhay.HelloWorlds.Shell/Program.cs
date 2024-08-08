ea663a0b9457397ee12da16b6104c22dc88c9361
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-08-08 13:14:56 -0700

    rebuild Songhay.HelloWorlds.Shell for [[dotnet|.NET 8.0]] 🚜✨ upgrade Songhay.HelloWorlds to [[dotnet|.NET 8.0]] 📦🔝

--- Songhay.HelloWorlds.Shell/Program.cs
+++ Songhay.HelloWorlds.Shell/Program.cs
@@ -1,27 +1,13 @@
-﻿using Songhay.HelloWorlds.Activities;
-using System.Reflection;
+﻿//using Songhay.HelloWorlds.Activities;
 
-namespace Songhay.HelloWorlds.Shell;
-
-static class Program
+static void DisplayCredits()
 {
-    static void DisplayCredits()
-    {
-        Console.Write(ProgramAssemblyUtility.GetAssemblyInfo(Assembly.GetExecutingAssembly(), true));
-        Console.WriteLine(string.Empty);
-        Console.WriteLine("Activities Assembly:");
-        Console.Write(ProgramAssemblyUtility.GetAssemblyInfo(typeof(TopTenActivity).Assembly, true));
-    }
-
-    static void Main(string[] args)
-    {
-        DisplayCredits();
-
-#if DEBUG
-        Console.WriteLine($"{Environment.NewLine}Press any key to continue...");
-        Console.ReadKey(false);
-#endif
-
-        Environment.Exit(0);
-    }
+    // Console.Write(ProgramAssemblyUtility.GetAssemblyInfo(Assembly.GetExecutingAssembly(), true));
+    // Console.WriteLine(string.Empty);
+    // Console.WriteLine("Activities Assembly:");
+    // Console.Write(ProgramAssemblyUtility.GetAssemblyInfo(typeof(TopTenActivity).Assembly, true));
 }
+
+DisplayCredits();
+
+Environment.Exit(0);
