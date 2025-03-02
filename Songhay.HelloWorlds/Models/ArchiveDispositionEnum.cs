e5431cf0a095cf03658eeb798c2f7b6ced8cce00
Bryan Wilhite <bryan.wilhite@gmail.com>
2025-03-01 19:01:31 -0800

    #14 download NASA Exoplanet Archive data into Songhay.HelloWorlds.Tests project 🚜⬇ Models/SpectralTypeEnum.cs 🚜✨ Models/ArchiveDispositionEnum.cs 🚜✨

---
+++ Songhay.HelloWorlds/Models/ArchiveDispositionEnum.cs
@@ -0,0 +1,16 @@
+/// <summary>
+/// Exoplanet Archive Disposition
+/// </summary>
+/// <remarks>
+/// Disposition updated by the Exoplanet Archive.
+///
+/// [ https://exoplanetarchive.ipac.caltech.edu/docs/API_k2pandc_columns.html ]
+/// </remarks>
+public enum ArchiveDispositionEnum
+{
+    CANDIDATE,
+    FALSE,
+    POSITIVE,
+    CONFIRMED,
+    REFUTED
+}
